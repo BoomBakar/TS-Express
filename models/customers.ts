aa79f048addd71cc58771b8ac8ee5dcd858273a5
Abubakar Dar <abubakar@xavor.com>
2023-08-11 17:19:03 +0500

    Models added

---
+++ models/customers.ts
@@ -0,0 +1,11 @@
+const customers: {id:number, name:string, age:number}[] = [
+    { id: 1, name: 'John', age: 25 },
+    { id: 2, name: 'Jane', age: 24 },
+    { id: 3, name: 'Jack', age: 26 },
+    { id: 4, name: 'Jill', age: 23 },
+    { id: 5, name: 'James', age: 22 },
+    { id: 6, name: 'Jenny', age: 21 },
+    { id: 7, name: 'Joe', age: 20 }
+];
+
+module.exports = customers;
--- router/route.ts
+++ router/route.ts
@@ -1,21 +1,45 @@
 import express from 'express';
-
 const router =  express.Router();
+//import customers from '../models/customers';
+const customers = require('../models/customers.ts');
 
-router.get('/', (req: express.Request, res:express.Response) => {
-    res.status(200).send('Hello bye World');
+//get customer list
+router.get('/customer', (req, res) => {
+    res.send(customers);
+    
 });
-//Home page
-router.get('/home', (req: express.Request, res:express.Response) => {
-    res.status(200).send('Home page');
+//get single customer
+router.get('/customer/:id', (req, res) => {
+    const customer = customers.find((c:any) => c.id === parseInt(req.params.id));
+    if(!customer) res.status(404).send('The customer with the given ID was not found.');
+    res.send(customer);
 });
-//About page
-router.get('/about', (req: express.Request, res:express.Response) => {
-    res.status(200).send('About page');
+//create new customer
+router.post('/customer', (req, res) => {
+    const customer = {
+        id: customers.length + 1,
+        name: req.body.name,
+        age: req.body.age
+    };
+    customers.push(customer);
+    res.send(customer);
 });
-//Contact page
-router.get('/contact', (req: express.Request, res:express.Response) => {
-    res.status(200).send('Contact page');
+//update customer
+router.put('/customer/:id', (req, res) => {
+    const customer = customers.find((c:any) => c.id === parseInt(req.params.id));
+    if(!customer) res.status(404).send('The customer with the given ID was not found.');
+    customer.name = req.body.name;
+    customer.age = req.body.age;
+    res.send(customer);
 });
+//delete customer
+router.delete('/customer/:id', (req, res) => {
+    const customer = customers.find((c:any) => c.id === parseInt(req.params.id));
+    if(!customer) res.status(404).send('The customer with the given ID was not found.');
+    const index = customers.indexOf(customer);
+    customers.splice(index, 1);
+    res.send(customer);
+});
+
 
 export default router;
